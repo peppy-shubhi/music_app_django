d6ef5fb7d2cb07cff06d959c14f16a41e321c286
peppy-shubhi <shubhikhandelwal98@gmail.com>
2018-02-10 15:02:48 +0530

    favorite bug removed by adm

--- music/views.py
+++ music/views.py
@@ -13,7 +13,7 @@ def detail(request, album_id):
 def favorite(request, album_id):
     album = get_object_or_404(Album, pk=album_id)
     try:
-        selected_song = album.song_set.get(pk=request.POST['song'])
+        selected_song = album.song_set.get(pk=int(request.POST['song']))
     except(KeyError, Song.DoesNotExist):
         return render(request,'music/detail.html',{
             'album': album,
